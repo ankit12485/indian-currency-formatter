adb2524c91785207af9d5fe49f949b218e512c6e
Shiva <shiva.gupta@krantitechservices.in>
2020-07-24 16:19:47 +0530

    Updated documentation and added decimal support

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
+# indian-currency-formatter
 This package transforms the amount into Indian Currency Format (INR)
+
+## Installation
+```
+npm install indian-currency-formatter
+```
+
+## Usage
+```
+import formatAmount from 'indian-currency-formatter';
+
+or
+
+const formatAmount = require('indian-currency-formatter');
+```
+
+## Usage
+```
+console.log(formatAmount(1500.20)); // 1,500.20
+console.log(formatAmount(5264210)); // 52,64,210
+```
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 function formatAmount(amount) {
-    amount = Math.round(amount);
+	//Split amount with decimal
     var result = amount.toString().split('.');
+    //Get last three digits of before decimal string
     var lastThree = result[0].substring(result[0].length - 3);
+    //Get other digits of before decimal string
     var otherNumbers = result[0].substring(0, result[0].length - 3);
+    //If there are other numbers then add a comma before lastThree
     if (otherNumbers != '' && otherNumbers != '-')
         lastThree = ',' + lastThree;
     var output = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
-    
     if (result.length > 1) {
         output += "." + result[1];
     }
